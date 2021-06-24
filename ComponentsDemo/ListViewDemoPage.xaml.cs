0322a12afc730323731ef46184404e6cef259ffd
Lutz <lambda2@gmx.at>
2021-06-24 12:37:58 +0200

    added demo for listview

---
+++ ComponentsDemo/ListViewDemoPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComponentsDemo
+{
+    /// <summary>
+    /// Interaction logic for ListViewDemoPage.xaml
+    /// </summary>
+    public partial class ListViewDemoPage : Page
+    {
+        private readonly List<ListElement> mListContent = new();
+
+        public ListViewDemoPage()
+        {
+            InitializeComponent();
+#if DEBUG
+            AddDebugStuff();
+#endif
+
+            mListContent.Add(new ListElement() { Name = "Hans", Number = 11 });
+            mListContent.Add(new ListElement() { Name = "Peter", Number = 22 });
+            mListContent.Add(new ListElement() { Name = "Ingrid", Number = 33 });
+            mListContent.Add(new ListElement() { Name = "Maria", Number = 44 });
+            lvContent.ItemsSource = mListContent;
+        }
+
+        private void AddDebugStuff()
+        {
+            Grid currentGrid = Content as Grid;
+            currentGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(25) });
+            Label debugLabel = new();
+            debugLabel.Content = "Debug-stuff";
+            Grid.SetRow(debugLabel, currentGrid.RowDefinitions.Count - 1);
+            Grid.SetColumn(debugLabel, 0);
+            currentGrid.Children.Add(debugLabel);
+        }
+
+        private void lvContent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tblContent.Inlines.Clear();
+            foreach (var item in lvContent.SelectedItems)
+            {
+                tblContent.Inlines.Add(new Run((item as ListElement).Name));
+                tblContent.Inlines.Add(new LineBreak());
+            }
+            
+        }
+    }
+
+    class ListElement
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int Number { get; set; }
+    }
+}
--- ComponentsDemo/MainWindow.xaml.cs
+++ ComponentsDemo/MainWindow.xaml.cs
@@ -20,64 +20,19 @@ namespace ComponentsDemo
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
 
-        private void btnStackpanel_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new StackpanelDemoPage());
-        }
-
-        private void btnGrid_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new GridDemoPage());
-        }
-
-        private void btnButton_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new ButtonDemoPage());
-        }
-
-        private void btnTextBox_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new TextBoxDemoPage());
-        }
-
-        private void btnTextBlock_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new TextBlockDemoPage());
-        }
-
-        private void btnImage_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new ImageDemoPage());
-        }
-
-        private void btnSlider_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new SliderDemoPage());
-        }
-
-        private void btnScrollBar_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new ScrollBarDemoPage());
-        }
-
-        private void btnColor_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new ColorDemoPage());
-        }
-
-        private void btnRadioCheck_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new RadioCheckDemoPage());
-        }
-
-        private void btnComboBox_Click(object sender, RoutedEventArgs e)
-        {
-            frmContent.Navigate(new ComboBoxDemoPage());
-        }
+        private void btnStackpanel_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new StackpanelDemoPage());
+        private void btnGrid_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new GridDemoPage());
+        private void btnButton_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new ButtonDemoPage());
+        private void btnTextBox_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new TextBoxDemoPage());
+        private void btnTextBlock_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new TextBlockDemoPage());
+        private void btnImage_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new ImageDemoPage());
+        private void btnSlider_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new SliderDemoPage());
+        private void btnScrollBar_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new ScrollBarDemoPage());
+        private void btnColor_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new ColorDemoPage());
+        private void btnRadioCheck_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new RadioCheckDemoPage());
+        private void btnComboBox_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new ComboBoxDemoPage());
+        private void btnListView_Click(object sender, RoutedEventArgs e) => frmContent.Navigate(new ListViewDemoPage());
     }
 }
