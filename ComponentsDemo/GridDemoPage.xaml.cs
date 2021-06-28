f02db6fb4945bd4b58599c813540294585b90d47
Lutz <lambda2@gmx.at>
2021-06-28 12:35:33 +0200

    added more grid examples

--- ComponentsDemo/GridDemoPage.xaml.cs
+++ ComponentsDemo/GridDemoPage.xaml.cs
@@ -24,5 +24,32 @@ namespace ComponentsDemo
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Button erstellen, da im XAML keine existieren
+            for (int counterRow = 0; counterRow < grdAlpha.RowDefinitions.Count; counterRow++)
+            {
+                for (int counterCol = 0; counterCol < grdAlpha.ColumnDefinitions.Count; counterCol++)
+                {
+                    // Button im RAM erstellen
+                    Button freshButton = new() 
+                        { Content = counterRow * grdAlpha.ColumnDefinitions.Count + counterCol};
+
+                    // Dem Grid die Position des Buttons mitteilen
+                    Grid.SetColumn(freshButton, counterCol);
+                    Grid.SetRow(freshButton, counterRow);
+                    // Dem Grid sagen das der erstellte Button ein Unterelement ist
+                    grdAlpha.Children.Add(freshButton);
+                }
+            }
+
+
+            foreach (Button item in grdAlpha.Children)
+            {
+                item.Background = Brushes.CornflowerBlue;
+            }
+        }
     }
 }
