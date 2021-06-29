86768a2f3e1129dfcd31978a429c06a9d376573e
Lutz <lambda2@gmx.at>
2021-06-29 14:40:06 +0200

    added comments

--- ComponentsDemo/Converter/ConverterSliderToLine.cs
+++ ComponentsDemo/Converter/ConverterSliderToLine.cs
@@ -4,17 +4,22 @@ using System.Windows.Data;
 
 namespace ComponentsDemo
 {
+    /// <summary>
+    /// Konvertiert Werte von 0 bis 10 zu 
+    /// </summary>
     [ValueConversion(typeof(double), typeof(double))]
     class ConverterSliderToLine : IValueConverter
     {
+        // pull
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return 300 - (double)value*30;
         }
 
+        // push
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return 300 - (double)value * 30;
         }
     }
 }
--- ComponentsDemo/FormattingExercisePage.xaml.cs
+++ ComponentsDemo/FormattingExercisePage.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Drawing; // nicht serienmässig dabei, kann aber über das Paket "System.Drawing.Common" hinzugefügt werden
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Drawing;
 
 namespace ComponentsDemo
 {
@@ -23,16 +12,27 @@ namespace ComponentsDemo
     {
         public FormattingExercisePage()
         {
-            InitializeComponent();
+           InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Methode um einen Screenshot der Anwendung zu machen und als "Screenshot.bmp" abzuspeichern
+        /// </summary>
+        /// <param name="sender">unused</param>
+        /// <param name="e">unused</param>
+        private void btnScreenshot_Click(object sender, RoutedEventArgs e)
         {
+            // Ein leeres Bitmap-Objekt erstellen mit der Anangsgrösse unseres Fensters
+            // Der Code ist innerhalb der Page, welche NICHT die fenstergrösse beeinflusst, sondern nur
+            // den Frame füllt, deshalb lesen wir die werte direkt vom MainWindow.
+            // Die Multiplikation mit 1.5 entsteht durch die Desktop-Skalierung welche bei mir 150% ist
+            // CopyFromScreen rechnet in Physikalischen Pixeln
+            // WPF rechnet in Logischen (plattformunabhängigen) Bild-Koordinaten welche nicht physischen pixeln entsprechen muss
             using (Bitmap bmp = new Bitmap((int)(Application.Current.MainWindow.Width * 1.5d), (int)(Application.Current.MainWindow.Height * 1.5d)))
             {
                 using (Graphics graphics = Graphics.FromImage(bmp))
                 {
-                    graphics.CopyFromScreen((int)(Application.Current.MainWindow.Left*1.5d), (int)(Application.Current.MainWindow.Top*1.5d), 0, 0, bmp.Size);
+                    graphics.CopyFromScreen((int)(Application.Current.MainWindow.Left * 1.5d), (int)(Application.Current.MainWindow.Top * 1.5d), 0, 0, bmp.Size);
                     bmp.Save("Screenshot.bmp");
                 }
             }
