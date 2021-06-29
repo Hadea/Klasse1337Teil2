3984f0dad10e7dc9412a726562d3fc54c286ef08
Lutz <lambda2@gmx.at>
2021-06-29 16:13:51 +0200

    added comments

--- ComponentsDemo/AudioDemoPage.xaml.cs
+++ ComponentsDemo/AudioDemoPage.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Media;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ComponentsDemo
 {
@@ -26,56 +16,86 @@ namespace ComponentsDemo
         public AudioDemoPage()
         {
             InitializeComponent();
+            // Inztanzieren des Mediaplayers
             mMusic = new();
+            
+            // Das Lied wird in dieser Demo nicht gewechselt sodass wir es direkt öffnen.
+            // Fehlgeschlagenes öffnen (Datei nicht vorhanden, etc) führt nicht zu einer
+            // Execption! Der Mediaplayer hat ein Event "MediaFailed" auf das reagiert
+            // werden kann wenn eine Datei nicht ladbar ist.
             mMusic.Open(new Uri("Audio/Kraddy-DubStep.mp3", UriKind.Relative));
+
+            // Das Event MediaEnded wird ausgelöst wenn das Lied bis zum Ende gespielt wurde.
+            // Hier könnte man entweder ein neues Lied laden (Playlist) oder, wie hier, das
+            // lied wiederholen lassen.
             mMusic.MediaEnded += loop;
+
+            // der Soundplayer kann nur einen einzigen WAV auf einmal ausgeben, egal wieviele
+            // Inztanzen der Klasse SoundPlayer existieren. Daher ist es nur für sehr kurze
+            // effekte brauchbar. Kann auch als <SoundPlayerAction /> in XAML benutzt werden
             mSound = new("Audio/Klick.wav");
+            
         }
 
+        /// <summary>
+        /// Event Handler für das MediaEnded Event des <see cref="MediaPlayer"/>.
+        /// Startet das Lied von vorn.
+        /// </summary>
+        /// <param name="sender">unbenutzt</param>
+        /// <param name="e">unbenutzt</param>
         private void loop(object sender, EventArgs e)
         {
             mMusic.Position = TimeSpan.Zero;
             mMusic.Play();
         }
 
+        /// <summary>
+        /// Event Handler um das geladene Lied abzuspielen
+        /// </summary>
+        /// <param name="sender">unbenutzt</param>
+        /// <param name="e">unbenutzt</param>
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
             mMusic.Play();
         }
 
+        /// <summary>
+        /// Event Handler um das geladene Lied zu pausieren
+        /// </summary>
+        /// <param name="sender">unbenutzt</param>
+        /// <param name="e">unbenutzt</param>
         private void btnPause_Click(object sender, RoutedEventArgs e)
         {
             mMusic.Pause();
         }
 
+        /// <summary>
+        /// Event Handler um das geladene Lied zu stoppen
+        /// </summary>
+        /// <param name="sender">unbenutzt</param>
+        /// <param name="e">unbenutzt</param>
         private void btnStop_Click(object sender, RoutedEventArgs e)
         {
             mMusic.Position = TimeSpan.Zero;
             mMusic.Pause();
         }
 
-        private void btnSoundPlayer_Click(object sender, RoutedEventArgs e)
-        {
-            mSound.Play();
-        }
+        /// <summary>
+        /// Event Handler um das geladene Lied ab der aktuellen position weiterzuspielen
+        /// </summary>
+        /// <param name="sender">unbenutzt</param>
+        /// <param name="e">unbenutzt</param>
+        private void btnSoundPlayer_Click(object sender, RoutedEventArgs e) => mSound.Play();
 
-        private void btnSystemA_Click(object sender, RoutedEventArgs e)
-        {
-            SystemSounds.Exclamation.Play();
-        }
+        #region Systemsounds
+        // Event Handler für SystemSounds
+        // Diese nutzen die im Betriebssystem eingestellten Standard-Sounds und benötigen deshalb keine
+        // Inztanz eines Players
 
-        private void btnSystemB_Click(object sender, RoutedEventArgs e)
-        {
-            SystemSounds.Asterisk.Play();
-        }
-
-        private void btnSystemC_Click(object sender, RoutedEventArgs e)
-        {
-            SystemSounds.Question.Play();
-        }
-        private void btnSystemD_Click(object sender, RoutedEventArgs e)
-        {
-            SystemSounds.Beep.Play();
-        }
+        private void btnSystemA_Click(object sender, RoutedEventArgs e) => SystemSounds.Exclamation.Play();
+        private void btnSystemB_Click(object sender, RoutedEventArgs e) => SystemSounds.Asterisk.Play();
+        private void btnSystemC_Click(object sender, RoutedEventArgs e) => SystemSounds.Question.Play();
+        private void btnSystemD_Click(object sender, RoutedEventArgs e) => SystemSounds.Beep.Play();
+        #endregion
     }
 }
