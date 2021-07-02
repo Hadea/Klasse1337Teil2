f6fc344d297e86550c106937153399bb69a772b9
Lutz <lambda2@gmx.at>
2021-07-02 09:16:06 +0200

    added opening of external browser for link

--- ComponentsDemo/TextBlockDemoPage.xaml.cs
+++ ComponentsDemo/TextBlockDemoPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,17 @@ namespace ComponentsDemo
             elementToAdd.Text = "Neues Element " + ++mAddedElementCounter;
             tblContent.Inlines.Add(new LineBreak());
             tblContent.Inlines.Add(elementToAdd);
+        }
+
+        private void Hyperlink_Click(object sender, RequestNavigateEventArgs e)
+        {
+            // startet einen neuen Prozess für die angegebene Datei. Anhand des Aufbaus des Dateinamens
+            // wird dann entschieden welches Programm die datei öffnet oder ob sie selbst startfähig ist
+            _ = Process.Start(new ProcessStartInfo() {FileName = e.Uri.ToString(), UseShellExecute = true });
+
+            // Um allen folgenden Eventhandlern zu signalisieren das das Event bereits erledigt ist
+            // wird das Handled auf true gesetzt. Folgende Eventhandler überspringen dann ihren code
+            e.Handled = true;
         }
     }
 }
