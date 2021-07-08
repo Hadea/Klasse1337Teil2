6153d2eccc5d78dd283a6eaee514dffc43da9dad
Lutz <lambda2@gmx.at>
2021-07-08 17:32:12 +0200

    added conditional styles

---
+++ ComponentsDemo/ConditionalStyleDemoPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComponentsDemo
+{
+    /// <summary>
+    /// Interaction logic for ConditionalStyleDemoPage.xaml
+    /// </summary>
+    public partial class ConditionalStyleDemoPage : Page, INotifyPropertyChanged
+    {
+
+        public bool RedAlert
+        {
+            get => _redAlert;
+            set
+            {
+                _redAlert = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(RedAlert)));
+            }
+        }
+        private bool _redAlert;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ConditionalStyleDemoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFlip_Click(object sender, RoutedEventArgs e)
+        {
+            RedAlert = !RedAlert;
+        }
+    }
+}
