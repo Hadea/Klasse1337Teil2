738b3a623db388e902ebc3c5e6d18c1012e0558f
Lutz <lambda2@gmx.at>
2021-07-12 10:09:00 +0200

    corrected generated numbers to reflect eurojackpot ruleset

--- ParallelDemo/Program.cs
+++ ParallelDemo/Program.cs
@@ -21,7 +21,7 @@ namespace ParallelDemo
              * Ausgabe der Statistik und anzahl der Ziehungen
              */
 
-            DauerSchein = new() { 3, 6, 12, 18, 21, 34, 41 };// ist sortiert, binärsuche?!
+            DauerSchein = new() { 12, 18, 21, 34, 41, 3, 6 };// ist sortiert, binärsuche?!
             int[] statistik = new int[8];
 
             // [0] 354891
@@ -79,9 +79,9 @@ namespace ParallelDemo
 
             // Ziehung   5 3 7 9 1 12 17
             // Schein    7 3 5 1 9 17 12
-            for (int i = 0; i < Ziehung.Count; i++)
+            for (int i = 0; i < 5; i++)
             {
-                for (int x = 0; x < DauerSchein.Count; x++)
+                for (int x = 0; x < 5; x++)
                 {
                     if (Ziehung[i] == DauerSchein[x])
                     {
@@ -89,20 +89,62 @@ namespace ParallelDemo
                     }
                 }
             }
+            
+            if (Ziehung[5] == DauerSchein[5] || Ziehung[5] == DauerSchein[6])// Ziehung 5 vergleichen mit Dauerschein 5 und 6
+                        rightNumberCount++;
+            if (Ziehung[6] == DauerSchein[5] || Ziehung[6] == DauerSchein[6])// Ziehung 6 vergleichen mit Dauerschein 5 und 6
+                        rightNumberCount++;
+
+            
+            // Alternative mit schleifen
+            /*for (int i = 5; i < 7; i++)
+            {
+                for (int x = 5; x < 7; x++)
+                {
+                    if (Ziehung[i] == DauerSchein[x])
+                    {
+                        rightNumberCount++;
+                    }
+                }
+            }*/
             return rightNumberCount;
         }
 
         public static List<byte> generateNumbers()
         {
             List<byte> Ziehung = new(7);
+
             Random rndGen = new();
             byte RandomNumber;
-            while (Ziehung.Count < 7)
+            while (Ziehung.Count < 5)
             {
-                RandomNumber = (byte)rndGen.Next(1, 50);
+                RandomNumber = (byte)rndGen.Next(1, 51); //ab 1 inclusive, unterhalb 51
                 if (!Ziehung.Contains(RandomNumber))
                     Ziehung.Add(RandomNumber);
             }
+
+
+            Ziehung.Add((byte)rndGen.Next(1, 11));//ab 1 inclusive, unterhalb 11
+
+            do
+            {
+                RandomNumber = (byte)rndGen.Next(1, 11);//ab 1 inclusive, unterhalb 11
+            }
+            while (RandomNumber == Ziehung[5]) ;
+
+            Ziehung.Add(RandomNumber);
+            /*
+             *  Erster weg die Superzahlen zu ziehen
+            List<byte> ZiehungSuperzahl = new(2);
+            while (ZiehungSuperzahl.Count < 2)
+            {
+                RandomNumber = (byte)rndGen.Next(1, 11);//ab 1 inclusive, unterhalb 11
+                if (!ZiehungSuperzahl.Contains(RandomNumber))
+                    ZiehungSuperzahl.Add(RandomNumber);
+            }
+
+            Ziehung.AddRange(ZiehungSuperzahl);
+            */
             return Ziehung;
         }
 
