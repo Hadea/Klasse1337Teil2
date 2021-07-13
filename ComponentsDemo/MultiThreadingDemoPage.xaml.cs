fd7cff2a8b5a7af5b38a5aea8465203acc32a352
Lutz <lambda2@gmx.at>
2021-07-13 12:57:43 +0200

    added progress reporting and cancellation

--- ComponentsDemo/MultiThreadingDemoPage.xaml.cs
+++ ComponentsDemo/MultiThreadingDemoPage.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ComponentsDemo
@@ -19,7 +13,7 @@ namespace ComponentsDemo
     /// <summary>
     /// Interaction logic for MultiThreadingDemoPage.xaml
     /// </summary>
-    public partial class MultiThreadingDemoPage : Page
+    public partial class MultiThreadingDemoPage : Page, INotifyPropertyChanged
     {
 
         // Fields für die Threads, falls wir später noch zugriff darauf brauchen. z.B. um nachzuschauen ob er fertig ist
@@ -28,9 +22,47 @@ namespace ComponentsDemo
         private Task threadC;
         private Task threadD;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly Progress<double> progressReporter;
+        private CancellationTokenSource sumCancellationTokenSource;
+        private double _progressOfSum;
+
+        public double ProgressOfSum
+        {
+            get => _progressOfSum;
+            set
+            {
+                if (_progressOfSum == value) return;
+                _progressOfSum = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ProgressOfSum)));
+            }
+        }
+
+        private long _resultOfSum;
+
+        public long ResultOfSum
+        {
+            get { return _resultOfSum; }
+            set
+            {
+                if (_resultOfSum == value) return;
+                _resultOfSum = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ResultOfSum)));
+            }
+        }
+
+
         public MultiThreadingDemoPage()
         {
             InitializeComponent();
+            progressReporter = new((x) => ProgressOfSum = x);
+            /* entspricht
+                private void namenlos(int x)
+                {
+                    ProgressOfSum = x;
+                }
+             */
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
@@ -64,6 +96,56 @@ namespace ComponentsDemo
             Thread.Sleep(rndGen.Next(3000, 6000)); // zufälliges warten zwischen 3 und 6 sekunden
             //RectangleToRecolor.Fill = Brushes.CornflowerBlue; // verboten! Keine direkten UI operationen aus Threads
             _ = Dispatcher.Invoke(() => RectangleToRecolor.Fill = Brushes.CornflowerBlue); // UI-Thread startet den Lambda, damit darf das UI geändert werden
+        }
+
+        private long RandomArraySum(IProgress<double> sumProgress, CancellationToken token)
+        {
+            byte[] arrayToSum = new byte[800_000_000];
+            Random rndGen = new();
+
+            int progresscounter = 0;
+            long sumOfArray = 0;
+            for (int counter = 0; counter < arrayToSum.Length; counter++)
+            {
+                arrayToSum[counter] = (byte)rndGen.Next(256);
+                if (counter % (arrayToSum.Length / 750) == 0)
+                {
+                    progresscounter++;
+                    sumProgress.Report(progresscounter);
+                    if (token.IsCancellationRequested)
+                    {
+                        return sumOfArray;
+                    }
+                }
+            }
+
+            for (int counter = 0; counter < arrayToSum.Length; counter++)
+            {
+                sumOfArray += arrayToSum[counter];
+                if (counter % (arrayToSum.Length / 250) == 0)
+                {
+                    progresscounter++;
+                    sumProgress.Report(progresscounter);
+                    if (token.IsCancellationRequested)
+                    {
+                        return sumOfArray;
+                    }
+                }
+            }
+
+            return sumOfArray;
+        }
+
+        private async void btnStartSumWithProgress_Click(object sender, RoutedEventArgs e)
+        {
+            ProgressOfSum = 0;
+            sumCancellationTokenSource = new();
+            ResultOfSum = await Task.Run(() => RandomArraySum(progressReporter, sumCancellationTokenSource.Token), sumCancellationTokenSource.Token);
+        }
+
+        private void btnStopSumWithProgress_Click(object sender, RoutedEventArgs e)
+        {
+            sumCancellationTokenSource.Cancel();
         }
     }
 }
