a408889a9a1bb4729281b361dc1e3610ba7ee0dc
Lutz <lambda2@gmx.at>
2021-07-26 12:19:45 +0200

    added content filter for datagrid

--- ComponentsDemo/DataGridDemoPage.xaml.cs
+++ ComponentsDemo/DataGridDemoPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,8 @@ namespace ComponentsDemo
     public partial class DataGridDemoPage : Page
     {
         public ObservableCollection<Content> ContentList { get; init; } = new();
+        private ICollectionView contentListView { get; init; }
+        public string FilterText { get; set; } = string.Empty; // beim start ein leerer string damit im Filter kein null-test nötig ist
         public DataGridDemoPage()
         {
             InitializeComponent();
@@ -31,6 +34,21 @@ namespace ComponentsDemo
             ContentList.Add(new Content() {Name = "Karen", IsReady = false });
             ContentList.Add(new Content() {Name = "Kevin", IsReady = false });
             ContentList.Add(new Content() {Name = "Petra", IsReady = true });
+
+            // erstellt eine Ansicht für eine Collection mit der wir rein visuell die Inhalte manipulieren und filtern können
+            // die originaldaten werden dabei nicht angetastet
+            contentListView = CollectionViewSource.GetDefaultView(ContentList);
+
+            // Zum filtern wird hier ein Lambda verwendet welches für jedes element in der Collection gestartet wird
+            // sobald der Befehl Refresh gestartet wurde.
+            // Dieser filter nutzt den Inhalt eines Properties (durch TextBox gebunden und befüllt) mit dem es vergleicht
+            // und bool zurückgibt. Wenn der bool true ist wird das Element angezeigt, andernfalls nicht.
+            contentListView.Filter = new Predicate<object>(x => (x as Content).Name.Contains(FilterText));
+        }
+
+        private void tbxSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            contentListView.Refresh(); //aktualisiert die ansicht und wendet dabei die filter an
         }
     }
 
